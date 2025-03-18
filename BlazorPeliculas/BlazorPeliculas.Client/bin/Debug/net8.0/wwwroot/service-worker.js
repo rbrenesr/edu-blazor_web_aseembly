2276eb1fcdb962a12261261f72d023e53a1fb50e
Rafael Brenes <rbrenesr@gmail.com>
2025-03-18 06:50:29 -0600

    feat(project) Blazor Peliculas added _rb

---
+++ BlazorPeliculas/BlazorPeliculas.Client/bin/Debug/net8.0/wwwroot/service-worker.js
@@ -0,0 +1,5 @@
+/* Manifest version: K8XpeGaC */
+// In development, always fetch from the network and do not enable offline support.
+// This is because caching would make development more difficult (changes would not
+// be reflected on the first load after each change).
+self.addEventListener('fetch', () => { });
