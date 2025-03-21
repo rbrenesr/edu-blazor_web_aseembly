df80e3235668548ee87f55132accb9b82e785c15
Rafael Brenes <rbrenesr@gmail.com>
2025-03-20 22:56:20 -0600

    33. Inyección de Dependencias _rb

--- BlazorPeliculas/BlazorPeliculas.Client/Program.cs
+++ BlazorPeliculas/BlazorPeliculas.Client/Program.cs
@@ -7,5 +7,12 @@ builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+ConfigureServices(builder.Services);
 
 await builder.Build().RunAsync();
+
+void ConfigureServices(IServiceCollection services)
+{
+    services.AddSingleton<ServicioSingleton>();
+    services.AddTransient<ServicioTrasient>();
+}
---
+++ BlazorPeliculas/BlazorPeliculas.Client/Servicios.cs
@@ -0,0 +1,13 @@
+﻿namespace BlazorPeliculas.Client
+{
+    public class ServicioSingleton
+    {
+        public int Valor { get; set; }
+    }
+
+    public class ServicioTrasient
+    {
+        public int Valor { get; set; }
+    }
+
+}
