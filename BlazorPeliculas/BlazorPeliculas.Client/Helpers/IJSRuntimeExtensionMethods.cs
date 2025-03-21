f5562b9affca410f428bcd33015d0f85b93a035e
Rafael Brenes <rbrenesr@gmail.com>
2025-03-21 07:13:00 -0600

    38. Invocar métodos de Javascript desde c# _rb

---
+++ BlazorPeliculas/BlazorPeliculas.Client/Helpers/IJSRuntimeExtensionMethods.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.JSInterop;
+
+namespace BlazorPeliculas.Client.Helpers
+{
+    public static class IJSRuntimeExtensionMethods
+    {
+        public static async ValueTask<bool> Confirm(this IJSRuntime js, string mensaje)
+        {
+
+            await js.InvokeVoidAsync("console.log", "prueba de console log");
+
+            return await js.InvokeAsync<bool>("confirm", mensaje);
+        }
+    }
+}
