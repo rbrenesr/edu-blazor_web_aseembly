456b168f8cd20e5d3388468333f41ecf966a6334
Rafael Brenes <rbrenesr@gmail.com>
2025-03-21 06:55:36 -0600

    36. Clases Parciales - Separando Código de C# y UI _rb

---
+++ BlazorPeliculas/BlazorPeliculas.Client/Pages/Counter.razor.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Components;
+
+namespace BlazorPeliculas.Client.Pages
+{
+    public partial class Counter
+    {
+        [Inject] ServicioSingleton Singleton { get; set; } = null!;
+        [Inject] ServicioTrasient Transient { get; set; } = null!;
+
+        private int currentCount = 0;
+
+        private void IncrementCount()
+        {
+            currentCount++;
+            Singleton.Valor = currentCount;
+            Transient.Valor = currentCount;
+        }
+
+    }
+}
