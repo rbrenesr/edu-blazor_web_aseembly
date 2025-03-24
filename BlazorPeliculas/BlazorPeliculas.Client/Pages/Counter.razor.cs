8e9242a31d3d1894bdce223a40dc40e0e923a2d3
Rafael Brenes <rbrenesr@gmail.com>
2025-03-24 15:21:19 -0600

    41.Aislamiento de JavaScript _rb

--- BlazorPeliculas/BlazorPeliculas.Client/Pages/Counter.razor.cs
+++ BlazorPeliculas/BlazorPeliculas.Client/Pages/Counter.razor.cs
@@ -9,17 +9,25 @@ namespace BlazorPeliculas.Client.Pages
         [Inject] ServicioTrasient Transient { get; set; } = null!;
 
         [Inject] IJSRuntime JS { get; set; } = null!;
+        IJSObjectReference modulo;
+
+
 
         private int currentCount = 0;
         private static int currentCountStatic = 0;
 
         private async Task IncrementCount()
         {
+            modulo = await JS.InvokeAsync<IJSObjectReference>("import", "./js/Counter.js");
+            await modulo.InvokeVoidAsync("mostrarAlerta", "Hola mundo");
+
             currentCount++;
             currentCountStatic++;
             Singleton.Valor = currentCount;
             Transient.Valor = currentCount;
             await JS.InvokeVoidAsync("pruebaPuntoNetStatic");
+
+
         }
 
         [JSInvokable]
