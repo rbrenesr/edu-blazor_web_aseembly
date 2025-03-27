36f595312326ac71acc97ab194780ccbfb3f1244
Rafael Brenes <rbrenesr@gmail.com>
2025-03-26 22:15:40 -0600

    54.Lazy Loading - Postergando la carga de los DLLS's _rb

--- BlazorPeliculas/BlazorPeliculas.Client/Pages/Counter.razor.cs
+++ BlazorPeliculas/BlazorPeliculas.Client/Pages/Counter.razor.cs
@@ -1,15 +1,28 @@
 ﻿using BlazorPeliculas.Client.Helpers;
+using MathNet.Numerics.Statistics;
 using Microsoft.AspNetCore.Components;
 using Microsoft.JSInterop;
+using System.Xml.Xsl;
 
 namespace BlazorPeliculas.Client.Pages
 {
     public partial class Counter
     {
         private int currentCount = 0;
-        private void IncrementCount()
-        {      
-           currentCount++;
+        [Inject] public IJSRuntime js { get; set; } = null!;
+
+
+        private async Task IncrementCount()
+        {     
+            
+
+            var arreglo = new double[] { 1, 2, 3, 4, 5 };
+            var max = arreglo.Maximum();
+            var min = arreglo.Minimum();
+
+            js.InvokeVoidAsync("alert", $"El max es {max}, el min es {min}");
+
+            currentCount++;
         }
     }
 }
